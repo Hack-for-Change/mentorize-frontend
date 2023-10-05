84cfbf0118142b5664b6af53eef60c5a60afb18a
Tomaz <tomazmppontes@gmail.com>
2023-10-05 17:02:23 -0300

    aaaaaaaaaaaaaaaaaa

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 
 const app = express();
@@ -6,10 +7,31 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, 'mentorize-frontend')));
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/mentorize-frontend/index.html');
+  res.sendFile(path.join(__dirname, 'mentorize-frontend', 'index.html'));
 });
 
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'mentorize-frontend', 'login.html'));
+});
+
+app.get('/cadastro', (req, res) => {
+  res.sendFile(path.join(__dirname, 'mentorize-frontend', 'cadastro.html'));
+});
+
+app.get('/sobrenos', (req, res) => {
+  res.sendFile(path.join(__dirname, 'mentorize-frontend', 'sobrenos.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'mentorize-frontend', 'mentor.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'mentorize-frontend', 'aluno.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
